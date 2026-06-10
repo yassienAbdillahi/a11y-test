9fe9e3eafcc7a8c46678f40987a10b7edf915e20
Yassien Abdillahi <yassien.abdillahi@tpximpact.com>
2026-06-10 16:45:36 +0100

    feat: fail wcag 1.3.3 Sensory Characteristics

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 console.log("js loaded");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const email = document.getElementById('email');
+
+        if (email.value.trim() === '') {
+            alert('Please complete all required fields.');
+        }
+    });
+});
